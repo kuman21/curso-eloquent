639ac8c97ae50465a7adc0a398a71ad214254f50
Victor Tovar <victog21@outlook.com>
2020-03-02 19:00:25 -0600

    Se añadieron las relaciones faltantes en los modelos

--- app/Category.php
+++ app/Category.php
@@ -6,5 +6,14 @@ use Illuminate\Database\Eloquent\Model;
 
 class Category extends Model
 {
-    //
+    // Una categoria tiene muchos posts
+    public function posts()
+    {
+        return $this->hasMany(Post::class);
+    }
+
+    public function videos()
+    {
+        return $this->hasMany(Video::class);
+    }
 }
--- app/Post.php
+++ app/Post.php
@@ -6,5 +6,31 @@ use Illuminate\Database\Eloquent\Model;
 
 class Post extends Model
 {
-    //
+    public function user()
+    {
+        return $this->belongsTo(User::class);
+    }
+
+    public function category()
+    {
+        return $this->belongsTo(Category::class);
+    }
+
+    // Uno a muchos
+    public function comments()
+    {
+        return $this->morphMany(Comment::class, 'commentable');
+    }
+
+    // Uno a uno
+    public function image()
+    {
+        return $this->morphOne(Image::class, 'imageable');
+    }
+
+    // Muchos a muchos
+    public function tags()
+    {
+        return $this->morphToMany(Tag::class, 'taggable');
+    }
 }
